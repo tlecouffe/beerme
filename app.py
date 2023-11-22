9e694579a74067260469e0552cb6897aa6cda3ce
Tyler LeCouffe <tlecouffe@hotmail.com>
2023-11-21 22:35:11 -0600

    commit - added comments, images, super basic registration - all SQLite

--- app.py
+++ app.py
@@ -1,4 +1,63 @@
 import streamlit as st
+import os
+import sqlite3
+from datetime import datetime
+
+# Ensure the directory for storing images exists
+os.makedirs('uploaded_images', exist_ok=True)
+
+# Initialize the database connection
+conn = sqlite3.connect('beer_comments.db', check_same_thread=False)
+c = conn.cursor()
+
+# Create table for users
+c.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY
+    )
+''')
+conn.commit()
+
+# Create table for comments with an image path column
+c.execute('''
+    CREATE TABLE IF NOT EXISTS comments (
+        beer_name TEXT,
+        username TEXT,
+        comment TEXT,
+        timestamp DATETIME,
+        image_path TEXT
+    )
+''')
+conn.commit()
+
+def get_user():
+    """Retrieve or register the user."""
+    # Check if a username is in the query params
+    query_params = st.experimental_get_query_params()
+    if 'username' in query_params:
+        username = query_params['username'][0]
+        st.session_state['username'] = username
+        return username
+
+    # User registration
+    username = st.text_input("Enter your name to continue:")
+    if username and st.button("Register/Login"):
+        c.execute("SELECT username FROM users WHERE username = ?", (username,))
+        if not c.fetchone():  # User doesn't exist, register new user
+            c.execute("INSERT INTO users (username) VALUES (?)", (username,))
+            conn.commit()
+        st.session_state['username'] = username
+        st.experimental_set_query_params(username=username)  # Set username in query params
+        return username
+
+    return None
+
+# Rest of your app's code...
+
+username = get_user()
+if username is None:
+    st.stop()
+
 
 # Data structure
 beers_info = {
@@ -44,6 +103,11 @@ beers_info = {
     }
 }
 
+# Sample data structure for flights
+flights = {
+    "Flight 1: The Classics": ["Beer 1", "Beer 2", "Beer 3", "Beer 4"],
+    "Flight 2: Bold & Adventurous": ["Beer 5", "Beer 6", "Beer 7", "Beer 8"]
+}
 
 # st.title("The Brewed Affair")
 # st.subheader("Beer Tasting Extravaganza")
@@ -56,9 +120,6 @@ def local_css(file_name):
 
 local_css("style.css")
 
-
-
-
 # Function to display beer details
 def show_beer_details(beer_name):
     beer = beers_info[beer_name]
@@ -70,6 +131,35 @@ def show_beer_details(beer_name):
         st.image(beer["primary_image"], width=300)  # Adjust width as needed for the primary image
 
     st.write(beer["description"])
+
+    # Comment Section with Image Upload
+    user_comment = st.text_area("Leave a comment:")
+    user_image = st.file_uploader("Upload an image:", type=["jpg", "png"])
+
+    if st.button('Post Comment', key=f'post_{beer_name}'):
+        timestamp = datetime.now()
+        image_path = None
+        if user_image is not None:
+            image_path = save_uploaded_image(user_image, beer_name)
+        c.execute("INSERT INTO comments (beer_name, username, comment, timestamp, image_path) VALUES (?, ?, ?, ?, ?)",
+                  (beer_name, st.session_state['username'], user_comment, timestamp, image_path))
+        conn.commit()
+        st.success("Comment and image added successfully!")
+
+    # Display existing comments and images
+    c.execute("SELECT username, comment, timestamp, image_path FROM comments WHERE beer_name = ? ORDER BY timestamp DESC", (beer_name,))
+    for username, comment, timestamp, image_path in c.fetchall():
+        st.markdown(f"*{username} ({timestamp}):*")
+        st.text_area("", comment, disabled=True)
+        if image_path:
+            st.image(image_path)
+
+def save_uploaded_image(uploaded_image, beer_name):
+    # Save the image and return its path
+    image_path = os.path.join('uploaded_images', f"{beer_name}_{uploaded_image.name}")
+    with open(image_path, "wb") as f:
+        f.write(uploaded_image.getbuffer())
+    return image_path
 
 # Function to display the main selection page
 def show_main_page():
@@ -92,12 +182,6 @@ def show_main_page():
         # Horizontal line for separation between flights
         st.markdown("---")
 
-
-# Sample data structure for flights
-flights = {
-    "Flight 1: The Classics": ["Beer 1", "Beer 2", "Beer 3", "Beer 4"],
-    "Flight 2: Bold & Adventurous": ["Beer 5", "Beer 6", "Beer 7", "Beer 8"]
-}
 
 # Main app logic
 if 'selected_beer' in st.session_state and st.session_state.selected_beer:
